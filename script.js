3c0ee23801739a68d848ca2e01d7c39503cfa80f
rodrigonoleto92 <noletonovo@gmail.com>
2025-11-10 16:07:23 -0300

    O "Alert" foi substituido por um H2

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function generationNumber(){
- 
-    const min=Math.ceil( document.querySelector('.input1').value)
-  const max=Math.floor(document.querySelector('.input2').value)
+function generationNumber() {
 
-  if( min >= max){
+  const min = Math.ceil(document.querySelector('.input1').value)
+  const max = Math.floor(document.querySelector('.input2').value)
+
+  if (min >= max) {
     alert("O valor minimo tem que ser MENOR que o valor maximo")
   }
-  else{
-    const result =Math.floor(Math.random() * (max - min) + min);
-
-  alert(result) 
+  else {
+    const result = Math.floor(Math.random() * (max - min) + min);
+    const h2 = document.getElementById("result");
+    h2.innerHTML = ` <span>${result}</span>`
   }
-  
-  
+
+
 }
